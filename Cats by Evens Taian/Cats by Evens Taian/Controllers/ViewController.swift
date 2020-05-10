2978c67fccb6ebe29490041d8002d50a292e74e5
Evens Taian <evenstaian@gmail.com>
2020-05-10 15:55:01 -0400

    init structure of project

---
+++ Cats by Evens Taian/Cats by Evens Taian/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cats by Evens Taian
+//
+//  Created by Evens Taian on 10/05/20.
+//  Copyright © 2020 Evens Taian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cats by Evens Taian/Cats by Evens TaianTests/Cats_by_Evens_TaianTests.swift
@@ -0,0 +1,34 @@
+//
+//  Cats_by_Evens_TaianTests.swift
+//  Cats by Evens TaianTests
+//
+//  Created by Evens Taian on 10/05/20.
+//  Copyright © 2020 Evens Taian. All rights reserved.
+//
+
+import XCTest
+@testable import Cats_by_Evens_Taian
+
+class Cats_by_Evens_TaianTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
